d2fd5fb6124aa772f29a4325b29a206f9ef98a45
Kennedy <kennedy.fpl22@gmail.com>
2020-02-12 22:03:23 -0300

    Added Flask and variables

---
+++ application.py
@@ -0,0 +1,27 @@
+from flask import Flask
+app= Flask(__name__)
+@app.route("/")
+def index():
+    return "Hello, world!"
+@app.route("/kennedy")
+def kennedy():
+    return "Hello, Kennedy!"
+@app.route("/<string:name>")
+def hello(name):
+    name=name.capitalize()
+    return f"Hello, {name}!"
+from flask import Flask, render_template
+app= Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+from flask import Flask, render_template
+app= Flask(__name__)
+@app.route("/")
+def index():
+    headline="Hello!"
+    return render_template("index.html", headline=headline)
+@app.route("/bye")
+def bye():
+    headline="Goodbye!"
+    return render_template("index.html", headline=headline)
